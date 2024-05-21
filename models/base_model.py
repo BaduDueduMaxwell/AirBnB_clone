afdf6e2dad8eb7eaa3c0f60dfb072be6f1bace3d
kode <duedumaxwell43@gmail.com>
2024-05-21 13:02:13 -0700

    task 4:Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -5,20 +5,27 @@ import uuid
 import datetime
 
 class BaseModel:
-    """defines all common attributes/methods for the other classes"""
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs):
+        """Initialization of the BaseModel instance."""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.fromisoformat(value))
+                elif key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """String representation of BaseModel class"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
-    
+
     def save(self):
         """updates attribute updated_at with current datetime"""
         self.updated_at = datetime.now()
-        
+
     def to_dict(self):
         """
         A method to convert the instance attributes to a dictionary format.
