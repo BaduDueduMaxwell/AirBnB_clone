0c686a4e5d12f7e8dcbb28d069c5a4ba2c0eb75a
kode <duedumaxwell43@gmail.com>
2024-05-21 13:04:44 -0700

    task 4:modification

--- models/base_model.py
+++ models/base_model.py
@@ -18,10 +18,6 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = self.created_at
 
-    def __str__(self):
-        """String representation of BaseModel class"""
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
-
     def save(self):
         """updates attribute updated_at with current datetime"""
         self.updated_at = datetime.now()
@@ -36,3 +32,7 @@ class BaseModel:
         instance_dict["created_at"] = self.created_at.isoformat()
         instance_dict["updated_at"] = self.updated_at.isoformat()
         return instance_dict
+
+    def __str__(self):
+        """String representation of BaseModel class"""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
