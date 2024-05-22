b899f7760c3109bd0a775b9090509202c5ab1cf4
Maxwell Duedu <102438296+BaduDueduMaxwell@users.noreply.github.com>
2024-05-22 23:24:21 +0000

    task 4:create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -3,25 +3,42 @@
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
     """
     BaseModel class defines common attributes/methods for other classes.
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initializes a new instance of the BaseModel class.
-        Assigns a unique id using uuid.uuid4() and sets created_at and updated_at to the current datetime.
+        If kwargs is not empty:
+            - Each key-value pair in kwargs represents an attribute name \
+              and its value.
+            - id and created_at are created as in the previous implementation.
+        Otherwise:
+            - id and created_at are generated as in the previous \
+              implementation.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key == 'created_at' or key == 'updated_at':
+                        setattr(\
+                self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """
         Returns a string representation of the BaseModel object.
         """
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(self.__class__.__name__, \
+                                self.id, self.__dict__)
 
     def save(self):
         """
@@ -40,15 +57,27 @@ class BaseModel:
         obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
 
+
 if __name__ == "__main__":
     my_model = BaseModel()
-    my_model.name = "My First Model"
+    my_model.name = "My_First_Model"
     my_model.my_number = 89
+    print(my_model.id)
     print(my_model)
-    my_model.save()
-    print(my_model)
+    print(type(my_model.created_at))
+    print("--")
     my_model_json = my_model.to_dict()
     print(my_model_json)
     print("JSON of my_model:")
-    for key, value in my_model_json.items():
-        print("\t{}: ({}) - {}".format(key, type(value), value))
+    for key in my_model_json.keys():
+        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), \
+                                       my_model_json[key]))
+
+    print("--")
+    my_new_model = BaseModel(**my_model_json)
+    print(my_new_model.id)
+    print(my_new_model)
+    print(type(my_new_model.created_at))
+
+    print("--")
+    print(my_model is my_new_model)
