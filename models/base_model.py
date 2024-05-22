657627c1950bdbaf0f53b197802557ebf7f3173e
Maxwell Duedu <102438296+BaduDueduMaxwell@users.noreply.github.com>
2024-05-22 23:17:58 +0000

    task 3:BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -2,52 +2,32 @@
 
 import uuid
 from datetime import datetime
-from models import storage
-
 
 class BaseModel:
     """
     BaseModel class defines common attributes/methods for other classes.
     """
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
         """
         Initializes a new instance of the BaseModel class.
-        If kwargs is not empty:
-            - Each key-value pair in kwargs represents an attribute name \
-              and its value.
-            - id and created_at are created as in the previous implementation.
-        Otherwise:
-            - id and created_at are generated as in the previous \
-              implementation.
+        Assigns a unique id using uuid.uuid4() and sets created_at and updated_at to the current datetime.
         """
-        if kwargs:
-            for key, value in kwargs.items():
-                if key != '__class__':
-                    if key == 'created_at' or key == 'updated_at':
-                        setattr(self, key, datetime.strptime(value, \
-                                                "%Y-%m-%dT%H:%M:%S.%f"))
-                    else:
-                        setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
-            storage.new(self)
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """
         Returns a string representation of the BaseModel object.
         """
-        return "[{}] ({}) {}".format\
-            (self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """
         Updates the updated_at attribute with the current datetime.
         """
         self.updated_at = datetime.now()
-        storage.save()
 
     def to_dict(self):
         """
@@ -62,24 +42,13 @@ class BaseModel:
 
 if __name__ == "__main__":
     my_model = BaseModel()
-    my_model.name = "My_First_Model"
+    my_model.name = "My First Model"
     my_model.my_number = 89
-    print(my_model.id)
     print(my_model)
-    print(type(my_model.created_at))
-    print("--")
+    my_model.save()
+    print(my_model)
     my_model_json = my_model.to_dict()
     print(my_model_json)
     print("JSON of my_model:")
-    for key in my_model_json.keys():
-        print("\t{}: ({}) - {}".format\
-              (key, type(my_model_json[key]), my_model_json[key]))
-
-    print("--")
-    my_new_model = BaseModel(**my_model_json)
-    print(my_new_model.id)
-    print(my_new_model)
-    print(type(my_new_model.created_at))
-
-    print("--")
-    print(my_model is my_new_model)
+    for key, value in my_model_json.items():
+        print("\t{}: ({}) - {}".format(key, type(value), value))
