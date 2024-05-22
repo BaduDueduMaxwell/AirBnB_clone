517b44e50d0819929434a9bce46157f74f8111b7
kode <duedumaxwell43@gmail.com>
2024-05-21 20:22:16 -0700

    task 7:console 0.1

--- console.py
+++ console.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 import cmd
+from models.base_model import BaseModel
+from models import storage
+
 
 class HBNBCommand(cmd.Cmd):
     """
@@ -32,6 +35,96 @@ class HBNBCommand(cmd.Cmd):
         Do nothing on empty line
         """
         pass
+
+    def do_create(self, arg):
+        """
+        Creates a new instance of BaseModel, saves it (to the JSON file) and prints the id.
+        """
+        if not arg:
+            print("** class name missing **")
+        elif arg not in globals():
+            print("** class doesn't exist **")
+        else:
+            new_instance = globals()[arg]()
+            new_instance.save()
+            print(new_instance.id)
+
+    def do_show(self, arg):
+        """
+        Prints the string representation of an instance based on the class name and id.
+        """
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+        elif args[0] not in globals():
+            print("** class doesn't exist **")
+        elif len(args) < 2:
+            print("** instance id missing **")
+        else:
+            key = args[0] + '.' + args[1]
+            obj_dict = storage.all()
+            if key in obj_dict:
+                print(obj_dict[key])
+            else:
+                print("** no instance found **")
+
+    def do_destroy(self, arg):
+        """
+        Deletes an instance based on the class name and id (save the change into the JSON file).
+        """
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+        elif args[0] not in globals():
+            print("** class doesn't exist **")
+        elif len(args) < 2:
+            print("** instance id missing **")
+        else:
+            key = args[0] + '.' + args[1]
+            obj_dict = storage.all()
+            if key in obj_dict:
+                del obj_dict[key]
+                storage.save()
+            else:
+                print("** no instance found **")
+
+    def do_all(self, arg):
+        """
+        Prints all string representation of all instances based or not on the class name.
+        """
+        obj_dict = storage.all()
+        if not arg:
+            print([str(obj_dict[key]) for key in obj_dict])
+        elif arg not in globals():
+            print("** class doesn't exist **")
+        else:
+            print([str(obj_dict[key]) for key in obj_dict if key.startswith(arg + '.')])
+
+    def do_update(self, arg):
+        """
+        Updates an instance based on the class name and id by adding or updating attribute.
+        """
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+        elif args[0] not in globals():
+            print("** class doesn't exist **")
+        elif len(args) < 2:
+            print("** instance id missing **")
+        elif len(args) < 3:
+            print("** attribute name missing **")
+        elif len(args) < 4:
+            print("** value missing **")
+        else:
+            key = args[0] + '.' + args[1]
+            obj_dict = storage.all()
+            if key in obj_dict:
+                instance = obj_dict[key]
+                setattr(instance, args[2], args[3])
+                instance.save()
+            else:
+                print("** no instance found **")
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
