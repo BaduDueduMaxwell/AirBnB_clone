3ba2bad124debe9b8eca2d05e757a8c480014e0a
walkingkode <duedumaxwell43@gmail.com>
2024-08-31 16:36:00 -0700

    task 5:added models

--- models/base_model.py
+++ models/base_model.py
@@ -32,6 +32,8 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            from models import storage
+            storage.new(self)
 
     def __str__(self):
         """
@@ -45,6 +47,8 @@ class BaseModel:
         Updates the updated_at attribute with the current datetime.
         """
         self.updated_at = datetime.now()
+        from models import storage
+        storage.save()
 
     def to_dict(self):
         """
